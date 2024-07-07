f698f3952698dcbeecc168ef4a175fbb063138a2
pothaganilearning <pothagani.learning@gmail.com>
2024-07-07 11:56:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -160,7 +160,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function fetchQuizData() {
-        fetch('real3.json')
+        fetch('real4.json')
             .then(response => response.json())
             .then(data => {
                 quizData = data;
